246e0a74ada8fb2387c4d0bd2cfd6219b1a2bec1
manhit15 <manhit96@gmail.com>
2015-10-12 20:11:57 +0700

    Push N920T (1BOH4) Opensource

---
+++ TMB/drivers/media/platform/exynos/fimc-is2/vendor/fimc-is-vendor-config_zero.h
@@ -0,0 +1,15 @@
+#ifndef FIMC_IS_VENDOR_CONFIG_ZERO_H
+#define FIMC_IS_VENDOR_CONFIG_ZERO_H
+
+#include "fimc-is-from-rear-c2-imx240_v003.h"
+#include "fimc-is-eeprom-front-c2-4e6_v003.h"
+
+#define CAMERA_SYSFS_V2
+#define CAMERA_MODULE_DUALIZE
+#define CONFIG_COMPANION_STANDBY_CRC
+#define CAMERA_OIS_DOM_UPDATE_VERSION 'E'
+#define CAMERA_OIS_SEC_UPDATE_VERSION 'F'
+
+#define HOST_FW_INTERFACE_VER 1
+
+#endif /* FIMC_IS_VENDOR_CONFIG_ZERO_H */
